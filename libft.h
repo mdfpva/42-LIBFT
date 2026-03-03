73b25ea13e041c35b1bca3755df026771ac54579
mdfpva <miguelvazandre@outlook.com>
2026-03-03 15:16:24 +0000

    libft.h

---
+++ libft.h
@@ -0,0 +1,14 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+// libc functions
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_strlen(const char *s);
+void    *ft_memset(void *p, int c, int length);
+int	ft_atoi(const char *str);
+
+#endif
