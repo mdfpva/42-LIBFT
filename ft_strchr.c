4357fee13043f9c5f0db028564c5df4dac948110
mdfpva <miguelvazandre@outlook.com>
2026-03-07 17:38:41 +0000

    CT

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,12 +1,12 @@
 char	*ft_strchr(const char *s, int c)
 {
-	while (1)
+	while (*s)
 	{
 		if (*s == c)
 			return ((char *)s);
-		if (*s == '\0')
-			return ((char *)s);
 		s++;
 	}
+	if (c == '\0')
+		return ((char *)s);
 	return (0);
 }
--- libft.h
+++ libft.h
@@ -14,7 +14,7 @@ int		ft_isprint(int c);
 int		ft_atoi(const char *str);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-char	*ft_strchar(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 void	*ft_memset(void *p, int c, size_t length);
 void	*ft_memcpy(void *dest, const void *src, size_t length);
 void	*ft_memmove(void *dest, const void *src, size_t length);
