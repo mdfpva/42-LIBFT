14b3d58e8df40dd9ec5c418902b55c913032ca85
mdfpva <miguelvazandre@outlook.com>
2026-03-14 18:34:37 +0000

    ft_strnstr.c

--- libft.h
+++ libft.h
@@ -18,6 +18,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 void	*ft_memset(void *p, int c, size_t length);
 void	*ft_memcpy(void *dest, const void *src, size_t length);
 void	*ft_memmove(void *dest, const void *src, size_t length);
