a200647d93750dc0922ca571a0c45cf909799b9f
mdfpva <miguelvazandre@outlook.com>
2026-04-06 19:42:10 +0100

    ft_split.c

---
+++ ft_split.c
@@ -0,0 +1,81 @@
+#include "libft.h"
+
+static int	ft_word_count(char const *str, char c)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != c && (i == 0 || str[i - 1] == c))
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+static char	*ft_splitter(char const *str, char c)
+{
+	char	*word;
+	int		i;
+	int		len;
+
+	len = 0;
+	while (str[len] && str[len] != c)
+		len++;
+	word = (char *)malloc(sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+static void	*ft_free_all(char **words, int j)
+{
+	while (j >= 0)
+	{
+		free(words[j]);
+		j--;
+	}
+	free(words);
+	return (NULL);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		j;
+	char	**words;
+
+	if (!s)
+		return (NULL);
+	words = (char **)malloc(sizeof(char *) * (ft_word_count(s, c) + 1));
+	if (!words)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			words[j] = ft_splitter(&s[i], c);
+			if (!words[j])
+				return (ft_free_all(words, j - 1));
+			while (s[i] && s[i] != c)
+				i++;
+			j++;
+		}
+		else
+			i++;
+	}
+	words[j] = NULL;
+	return (words);
+}
