3573313f90cd32bb560c6fc4ca17753ee8d0314b
mdfpva <miguelvazandre@outlook.com>
2026-04-08 19:42:15 +0100

    ft_striteri.c

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ SRCS = ft_isalpha.c ft_memcpy.c ft_strrchr.c \
 	ft_calloc.c ft_strdup.c \
 	ft_substr.c ft_strjoin.c \
 	ft_strtrim.c ft_split.c ft_itoa.c \
-	ft_strmapi.c
+	ft_strmapi.c ft_striteri.c
 OBJS = $(SRCS:.c=.o)
 
 CC = cc
--- libft.h
+++ libft.h
@@ -34,6 +34,7 @@ void	*ft_memmove(void *dest, const void *src, size_t length);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	ft_bzero(void *p, size_t length);
 void	*ft_calloc(size_t n, size_t size);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
