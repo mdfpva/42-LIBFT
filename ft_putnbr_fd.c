b3d794bcdfc2086a3bfda6e48afd12d7bd637112
mdfpva <miguelvazandre@outlook.com>
2026-04-09 19:40:15 +0100

    ft_putnbr_fd.c

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	*s;
+
+	s = ft_itoa(n);
+	if (!s)
+		return ;
+	ft_putstr_fd(s, fd);
+	free(s);
+}
--- libft.h
+++ libft.h
@@ -39,6 +39,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
