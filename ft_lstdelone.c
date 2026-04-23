79d0202b28b18b376454c3f41a12359172d59934
mdfpva <70178046+mdfpva@users.noreply.github.com>
2026-04-23 11:20:46 +0100

    Add files via upload

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -19,3 +19,50 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	del(lst -> content);
 	free(lst);
 }
+
+#include <stdio.h>
+
+void	print_list(t_list *lst)
+{
+	t_list	*current;
+
+	current = lst;
+	while (current)
+	{
+		printf("%p -> %s\n", (void *)current, (char *)(current->content));
+		current = current->next;
+	}
+	printf("NULL\n");
+}
+
+int	main(void)
+{
+	t_list *lst = NULL;
+
+	t_list *node1 = ft_lstnew(ft_strdup("ONE"));
+	t_list *node2 = ft_lstnew(ft_strdup("TWO"));
+	t_list *node3 = ft_lstnew(ft_strdup("THREE"));
+
+	ft_lstadd_back(&lst, node1);
+	ft_lstadd_back(&lst, node2);
+	ft_lstadd_back(&lst, node3);
+
+	printf("Before deletion:\n");
+	print_list(lst);
+
+	// Relinking of nodes before deleting -> wouldn't be able to print otherwise
+	node1->next = node2->next;
+	ft_lstdelone(node2, free);
+
+	printf("\nAfter deletion:\n");
+	print_list(lst);
+
+	while (lst != NULL)
+	{
+		t_list *temp = lst;
+		lst = lst->next;
+		free(temp->content);
+		free(temp);
+	}
+	return (0);
+}
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mide-fre <mide-fre@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 09:12:16 by mide-fre          #+#    #+#             */
-/*   Updated: 2026/04/21 09:12:17 by mide-fre         ###   ########.fr       */
+/*   Updated: 2026/04/22 13:10:48 by mide-fre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,25 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*s;
-
-	s = ft_itoa(n);
-	if (!s)
-		return ;
-	ft_putstr_fd(s, fd);
-	free(s);
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			n *= -1;
+		}
+		if (n < 10)
+		{
+			ft_putchar_fd(n + 48, fd);
+		}
+		else
+		{
+			ft_putnbr_fd((n / 10), fd);
+			ft_putnbr_fd((n % 10), fd);
+		}
+	}
 }
